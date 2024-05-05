d7dd227d56ec837584b08bf98c747a2fe10a1773
zilfiana <119720354+zilfi31@users.noreply.github.com>
2024-05-05 20:18:23 +0800

    Add files via upload

---
+++ tugassentimen/main.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import joblib
+import time
+
+# define model requirement
+model =  joblib.load("/Volumes/Data/deployment/tugassentimen/naive_bayes.joblib")
+vectorizer = joblib.load("/Volumes/Data/deployment/tugassentimen/vectorizer.joblib")
+
+st.title("Sentimen Analisis Twitter Dataset")
+st.write("Sentimen Analisis dengan menggunakan dataset twwitter Pilkada Gubernur DKI Jakarta tahun 2017")
+
+
+with st.container():
+   sentences = st.text_area("Kalimat untuk dianalisis")
+   button = st.button("Prediksi")
+
+
+with st.container(border=True):
+   if(button):
+      with st.spinner('Wait for it...'):
+
+         # st.write(type(sentences))
+
+         # ubah kalimat ke angka menggunakan vectorizer
+         vectorized = vectorizer.transform([sentences])
+
+         # prediksi
+         predicted = model.predict(vectorized)[0]
+
+         # hitung probabilitas
+         probabilities = model.predict_proba(vectorized)[0]
+         probabilities = [f"{round(x*100, 2)}%" for x in probabilities]
+
+         probability = {
+            "Negative" : probabilities[0],
+            "Positive" : probabilities[1],
+         }
+         
+         time.sleep(2)
+
+         st.write(f"Prediksi : {predicted}")
+         st.write("Probabilitas : ")
+         st.table(probability)
+
+         
